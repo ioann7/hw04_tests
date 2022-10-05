05b3d075aa905df4bcf898b29a7dd52d1a7ed0f8
chakchakk <ioann.chimrov@gmail.com>
2022-10-05 10:16:39 +0300

    i am idiot

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -56,7 +56,7 @@ class PostPagesTests(TestCase):
     def test_post_not_in_unrelated_group(self):
         """Пост не попадает в группу к которой не принадлежит."""
         self.assertIn(self.post, self.group.posts.all())
-        self.assertNotIn(self.post, self.group_without_posts.posts)
+        self.assertNotIn(self.post, self.group_without_posts.posts.all())
 
     def test_created_post_is_displayed(self):
         """
